010246215b7cf578b4e3aa6805a72c3a41b2fec8
Danila Smirnov <ylptred@gmail.com>
2022-07-20 01:13:31 +0300

    Клиент

---
+++ client.py
@@ -0,0 +1,14 @@
+import urllib.request
+
+
+fp = urllib.request.urlopen("http://localhost:1234/")
+
+
+encodedContent = fp.read()
+decodedContent = encodedContent.decode("utf8")
+
+
+print(decodedContent)
+
+
+fp.close()
